c64b763d1be242a3bede98a37c0d3f245ce20f48
mouyleng <mouyleng@instedd.org>
2018-02-27 16:51:31 +0700

    control hardware backbutton

--- www/js/app/helper/dialog.js
+++ www/js/app/helper/dialog.js
@@ -1,10 +1,12 @@
 var Dialog = {
   closeDialog: function(element) {
     $("#" + element).hide();
+    App.dialogPageId = '';
     $.mobile.activePage.removeClass('ui-disabled');
   },
   showDialog:function(element){
     $("#" + element).show();
+    App.dialogPageId = element;
     $.mobile.activePage.addClass('ui-disabled');
   }
 };
